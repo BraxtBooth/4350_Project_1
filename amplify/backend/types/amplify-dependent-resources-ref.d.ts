a58c78815f7cd32972c503bb08b573f89da44d6b
Braxton Booth <u1144811@utah.edu>
2022-04-25 20:34:28 -0600

    Added support for AWS Amplify and upload funcionality.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "4350project1dbb86b5b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "S3Room": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
